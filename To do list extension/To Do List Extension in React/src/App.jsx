0d75800dc9411c090868943df8ba277464bc7997
AlvisOu <oualvis@gmail.com>
2024-08-27 22:05:13 +1200

    Used vite to set up. Wrote index.html. Copied over the css file from the old js version

---
+++ To do list extension/To Do List Extension in React/src/App.jsx
@@ -0,0 +1,22 @@
+import InputContainer from "./components/InputContainer"
+import TaskContainer from "./components/TaskContainer"
+import React from "react"
+
+export default function App() {
+
+	const [tasks, setTasks] = React.useState([])
+
+	function addTask(newTask){
+		setTasks(prevTasks => [...prevTasks, newTask])
+	}
+
+	return (
+		<div className ="container">
+			<InputContainer 
+				addTask = {addTask}
+			/>
+			<TaskContainer />
+		</div>
+	)
+}
+
