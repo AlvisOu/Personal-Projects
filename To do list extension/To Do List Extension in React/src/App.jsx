8c453dbda2f27ffa03b43f0496e5abc061badb7e
AlvisOu <oualvis@gmail.com>
2024-11-17 13:22:49 -0500

    Started incroporating the frontend with the backend using axios. Able to retrieve hard-coded JSON data

--- To do list extension/To Do List Extension in React/src/App.jsx
+++ To do list extension/To Do List Extension in React/src/App.jsx
@@ -1,18 +1,32 @@
 import InputContainer from "./components/InputContainer"
 import TaskContainer from "./components/TaskContainer"
 import React from "react"
+import { useState, useEffect } from 'react';
+import api from './services/api';
 
 export default function App() {
 
-	const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"))
+	const [tasks, setTasks] = useState([]); // State to store the tasks
 
-    //state variable tasks is initialized depending on whether local storage returns null
-    const [tasks, setTasks] = tasksFromLocalStorage ? React.useState(tasksFromLocalStorage) : React.useState([])
+	useEffect(() => {
+		// Function to fetch tasks from the backend
+		const fetchTasks = async () => {
+			try {
+				const response = await api.get('/user/tasks');
+				setTasks(response.data.tasks);
+				console.log('Tasks fetched:', response.data.tasks);
+			} catch (error) {
+				console.error('Error fetching tasks:', error);
+			}
+		};
+
+		fetchTasks();
+	}, []);
    
     //When tasks array changes, update local storage
-    React.useEffect( () => {
-        localStorage.setItem("tasks", JSON.stringify(tasks))
-    }, [tasks])
+    // React.useEffect( () => {
+    //     localStorage.setItem("tasks", JSON.stringify(tasks))
+    // }, [tasks])
 
 	function addTask(newTask){
 		setTasks(prevTasks => [...prevTasks, newTask])
